ee0c7da78f1ef8986d938adf4f6b35696f15a35d
winter <luvwintrss@gmail.com>
2024-10-02 00:28:44 -0700

    UGHHHHHHHHHHHHH

---
+++ POOF/dying.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// typedef std::vector < std::pair < std::string, int>> pairlist_t;
+// typedef std::string text_t;
+//	using std::cout;
+
+//	number_t students = 6 - (5 + 4) * 3 / 2;
+
+	// addition + subtraction
+//	students++;
+// 	students = students - 1;
+//	students -= 1;
+//	students--;
+
+	// multiplication
+//	students = students * 200;
+//	students *= 200;
+
+	// dividing 
+//	students /= 6;
+
+	// dividing into groups ?? 
+
+// using text_t = std::string;
+// using number_t = int;
--- POOF/main.cpp
+++ POOF/main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 #include <vector>
 
-// typedef std::vector < std::pair < std::string, int>> pairlist_t;
-// typedef std::string text_t;
-
-using text_t = std::string;
-using number_t = int;
-
-
 int main() {
-	using std::cout;
-
-
-
-	text_t firstName = "Astrid";
-
-	cout << firstName << '\n';
 
 	return 0;
 }
